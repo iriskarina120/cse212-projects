c7cd1d637ff27bcf4ef0325f6ac5188d58eabf30
iriskarina120 <aya21001@byui.edu>
2025-09-06 22:59:15 +0200

    MultiplesOf and RotateListRight

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,13 +8,25 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // 1. Create an array named "results" with the same length as "length".
+        // 2. For i from 0 to length - 1, compute the multiple as (i + 1) * number.
+        // 3. Store each multiple in results[i].
+        // 4. Return results.
 
-        return []; // replace this return statement with your own
+        double[] results = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            results[i] = (i + 1) * number;
+        }
+        return results;
     }
+    // TODO Problem 1 Start
+    // Remember: Using comments in your program, write down your process for solving this problem
+    // step by step before you write the code. The plan should be clear enough that it could
+    // be implemented by another person.
+
+    // replace this return statement with your own
+
 
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
@@ -25,6 +37,17 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
+        // 1.Check that data is not null and that amount is between 1 and data.Count (inclusive).
+        // 2.Extract the last amount elements and store them in a variable.
+        // 3.Remove the extracted elements from the original list.
+        // 4.Insert the extracted elements at the beginning of the data list.
+
+        if (data.Count >= amount)
+        {
+            List<int> rotate = data.GetRange(data.Count - amount, amount);
+            data.RemoveRange(data.Count - amount, amount);
+            data.InsertRange(0, rotate);
+        }
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
